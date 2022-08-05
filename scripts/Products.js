0db1b6eaa8ce281618bf7c89aee6157031b54595
Jeremy Myers <echelonical@gmail.com>
2022-08-05 11:33:09 -0500

    add eventListener to products on brewedAwakenings

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,5 +1,30 @@
 import { getProducts } from "./database.js"
 
+
+//algorithm
+//1. add click Event Listener to product strings.
+//2.  When a product is clicked a popup alert will appear with name of product and price below it with dollarsign in front
+
+//declare click event listener
+document.addEventListener("click",
+    (clickEvent) => {
+        //store clickEvent.target in variable defined itemClicked
+        const itemClicked = clickEvent.target
+        //if product is present then create a const using the split method below to create an array and destructure the definition into productId variable...
+        if (itemClicked.id.startsWith("product")) {
+            const [, productId] = itemClicked.id.split("--");
+            //iterate the products object array imported from database
+            for (const product of products) {
+                if (product.id === parseInt(productId))
+                    window.alert(`          ${product.name}\n           $${product.price.toFixed(2)}`)
+            }
+        }
+    }
+)
+
+
+
+
 const products = getProducts()
 
 export const Products = () => {
